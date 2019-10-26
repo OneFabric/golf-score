fa065a67a6585ea41536a65ddcd97f231ff7e014
OneFabric <1neFabric@gmail.com>
2019-10-26 10:47:20 -0700

    More functionality added, along with 'alert's.

--- code/clientSide/main.js
+++ code/clientSide/main.js
@@ -1,4 +1,8 @@
-function submit(numberOfPlayers)
+function submit(numberOfPlayers, nameOfSession, numberOfHoles)
 {
-    alert("The number of players are: "+numberOfPlayers)
+    alert(
+        "The number of players are: "+numberOfPlayers+"\n"
+        + "The name of the session is: "+nameOfSession+"\n"
+        + "The number of Holes are: "+numberOfHoles
+        )
 }
